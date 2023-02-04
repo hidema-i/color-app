1719bd07bcc04d7bf037a604abf06f9cf70f23a1
ShuumaIwamoto <iwa06ki12da@gmail.com>
2023-02-04 17:41:49 +0900

    color-code-app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # color-app
+
+#### Values JS
+
+[values.js](https://github.com/noeldelgado/values.js)
---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+// import "./App.css";
+
+import { useState } from "react";
+import Values from "values.js";
+import SingleColor from "./SingleColor";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#ffffff").all(1));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(1);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
